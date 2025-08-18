427c88ba5246f1ef55b9cb51f06ce5aada863d29
Sam Taylor <2015samtaylor@gmail.com>
2025-08-18 16:29:01 -0500

    New hardcode table ref for budget_enrollment nums

--- models/production/enrollment_demographics/enrollment_demographics.sql
+++ models/production/enrollment_demographics/enrollment_demographics.sql
@@ -1,8 +1,35 @@
 {{ config(materialized='view', schema='views') }}
 
-WITH subgroup_counts AS (
+WITH total_students_cte AS (
     SELECT
         school_name,
+        year,
+        CAST(grade_level AS STRING) AS grade_level,
+        COUNT(*) AS total_students
+    FROM {{ source('views', 'student_to_teacher') }}
+    WHERE year = '25-26'
+    GROUP BY school_name, grade_level, year
+),
+actual_vs_budget AS (
+    SELECT
+        bec.school_name,
+        '25-26' AS year,
+        CAST(bec.grade_level AS STRING) AS grade_level,
+        '' AS subgroup,
+        '' AS demographic,
+        ts.total_students AS student_count,
+        CAST(bec.budgeted_enrollment AS INT64) AS budgeted_enrollment,
+        CAST(bec.budgeted_enrollment AS INT64) - SAFE_CAST(ts.total_students AS INT64) AS seats_remaining
+    FROM {{ source('enrollment', 'budgeted_enrollment_capacity_hardcode_8_18_25') }} AS bec
+    LEFT JOIN total_students_cte AS ts
+        ON bec.school_name = ts.school_name
+       AND CAST(bec.grade_level AS STRING) = ts.grade_level
+       AND ts.year = '25-26'
+),
+subgroup_counts AS (
+    SELECT
+        school_name,
+        year,
         CAST(grade_level AS STRING) AS grade_level,
         'race' AS subgroup,
         race AS demographic,
@@ -10,12 +37,14 @@ WITH subgroup_counts AS (
         CAST(NULL AS INT64) AS budgeted_enrollment,
         CAST(NULL AS INT64) AS seats_remaining
     FROM {{ source('views', 'student_to_teacher') }}
-    GROUP BY school_name, grade_level, race
+    WHERE year = '25-26'
+    GROUP BY school_name, year, grade_level, race
 
     UNION ALL
 
     SELECT
         school_name,
+        year,
         CAST(grade_level AS STRING) AS grade_level,
         'elastatus' AS subgroup,
         elastatus AS demographic,
@@ -23,12 +52,14 @@ WITH subgroup_counts AS (
         CAST(NULL AS INT64) AS budgeted_enrollment,
         CAST(NULL AS INT64) AS seats_remaining
     FROM {{ source('views', 'student_to_teacher') }}
-    GROUP BY school_name, grade_level, elastatus
+    WHERE year = '25-26'
+    GROUP BY school_name, year, grade_level, elastatus
 
     UNION ALL
 
     SELECT
         school_name,
+        year,
         CAST(grade_level AS STRING) AS grade_level,
         'sped_identifier' AS subgroup,
         sped_identifier AS demographic,
@@ -36,12 +67,14 @@ WITH subgroup_counts AS (
         CAST(NULL AS INT64) AS budgeted_enrollment,
         CAST(NULL AS INT64) AS seats_remaining
     FROM {{ source('views', 'student_to_teacher') }}
-    GROUP BY school_name, grade_level, sped_identifier
+    WHERE year = '25-26'
+    GROUP BY school_name, year, grade_level, sped_identifier
 
     UNION ALL
 
     SELECT
         school_name,
+        year,
         CAST(grade_level AS STRING) AS grade_level,
         'absenteeism_status' AS subgroup,
         absenteeism_status AS demographic,
@@ -49,12 +82,14 @@ WITH subgroup_counts AS (
         CAST(NULL AS INT64) AS budgeted_enrollment,
         CAST(NULL AS INT64) AS seats_remaining
     FROM {{ source('views', 'student_to_teacher') }}
-    GROUP BY school_name, grade_level, absenteeism_status
+    WHERE year = '25-26'
+    GROUP BY school_name, year, grade_level, absenteeism_status
 
     UNION ALL
 
     SELECT
         school_name,
+        year,
         CAST(grade_level AS STRING) AS grade_level,
         'frlstatus' AS subgroup,
         frlstatus AS demographic,
@@ -62,25 +97,16 @@ WITH subgroup_counts AS (
         CAST(NULL AS INT64) AS budgeted_enrollment,
         CAST(NULL AS INT64) AS seats_remaining
     FROM {{ source('views', 'student_to_teacher') }}
-    GROUP BY school_name, grade_level, frlstatus
+    WHERE year = '25-26'
+    GROUP BY school_name, year, grade_level, frlstatus
 )
 
-SELECT * 
-FROM subgroup_counts
-
+SELECT *
+FROM actual_vs_budget
 UNION ALL
-
-SELECT
-    school AS school_name,
-    CAST(grade AS STRING) AS grade_level,
-    '' AS subgroup,
-    '' AS demographic,
-    total_enrollment AS student_count,
-    CAST(budgeted_enrollment AS INT64) AS budgeted_enrollment,
-    CAST(seats_available AS INT64) AS seats_remaining
-FROM {{ source('enrollment', 'budgeted_enrollment_capacity') }}
-WHERE grade != 'total'
+SELECT *
+FROM subgroup_counts
+ORDER BY school_name, grade_level, subgroup, demographic
 
 
-
-
+--student count coming from student_to_teacher seats_remaining recalculated.
