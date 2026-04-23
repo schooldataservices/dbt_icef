fcffc236528f9d525ed4e2115f23e4e88f1193bf
Sam Taylor <2015samtaylor@gmail.com>
2026-04-23 09:10:01 -0500

    application pipeline updates

--- models/production/enrollment/application_pipeline.sql
+++ models/production/enrollment/application_pipeline.sql
@@ -3,6 +3,17 @@
     schema='enrollment'
 ) }}
 
--- Daily-refreshed replica of dbt_historical.application_pipeline in enrollment dataset.
-SELECT *
-FROM {{ source('dbt_historical', 'application_pipeline') }}
+-- Stacked union of historical application-pipeline tables from dbt_historical → enrollment.application_pipeline.
+-- `year` tags which school-year snapshot each row came from ('24-25' vs '25-26').
+
+SELECT
+  s.*,
+  '24-25' AS year
+FROM {{ source('dbt_historical', 'application_pipeline_2025') }} s
+
+UNION ALL
+
+SELECT
+  s.*,
+  '25-26' AS year
+FROM {{ source('dbt_historical', 'application_pipeline_2026') }} s
